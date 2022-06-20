3f7d9f24d6344714df8e41fb9f7390d450c82687
sandix34 <sandrinezocca@gmail.com>
2022-06-20 11:54:34 +0200

    setting up content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,24 @@
+import styles from "./Content.module.scss";
+
 function Content() {
-    return <div className="flex-fill b3">Content</div>;
+    return (
+        <div className="flex-fill container p-20">
+            <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+            <div className={`card p-20 ${ styles.contentCard }`}>
+                <div className={ styles.grid }>
+                    <div className={ styles.elem }>ELEMENT</div>
+                    <div className={ styles.elem }>ELEMENT</div>
+                    <div className={ styles.elem }>ELEMENT</div>
+                    <div className={ styles.elem }>ELEMENT</div>
+                    <div className={ styles.elem }>ELEMENT</div>
+                    <div className={ styles.elem }>ELEMENT</div>
+                    <div className={ styles.elem }>ELEMENT</div>
+                    <div className={ styles.elem }>ELEMENT</div>
+                </div>
+            </div>
+        </div>
+
+    );
 }
 
 export default Content;
